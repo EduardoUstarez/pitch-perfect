e5e36eacd0f178624a3865592df1626d2d2d507e
Carlos Eduardo Angulo Ustarez <carlos_angulo_az@hotmail.com>
2015-04-12 22:37:12 -0400

    Committed by Udacity

---
+++ Pitch Perfect/Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Carlos Eduardo Angulo Ustarez on 4/11/15.
+//  Copyright (c) 2015 Udacity. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
